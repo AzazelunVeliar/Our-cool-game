9fab945d0f5a82e371aa05c5245c739fb81e2773
Anton20042 <anton258041@gmail.com>
2025-05-14 14:08:50 +0300

    FourhtCommit1

--- Assets/Script/MainMenuButton/AudioService.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-public class AudioService : MonoBehaviour
-{
-    public void Initialize() { /* Инициализация аудио */ }
-    public void PlaySound(string id) { /* Воспроизведение звука */ }
-}
